ae26b1874c7f93867c83ad0c854f02ba627e85f5
Mark Lin <m2linchen@outlook.com>
2015-11-20 18:03:07 +0800

    Change the Code of "Populating Next Right Pointers in Each Node II"

--- algorithms/java/src/PopulatingNextRightPointersinEachNodeII/PopulatingNextRightPointersinEachNodeII.java
+++ algorithms/java/src/PopulatingNextRightPointersinEachNodeII/PopulatingNextRightPointersinEachNodeII.java
@@ -1,8 +1,36 @@
 package PopulatingNextRightPointersinEachNodeII;
 
-/**
- * Created by Chen on 2015/11/20.
- */
+// Source : https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+// Author : Mark Lin
+// Date   : 2015-11-20
+
+/**********************************************************************************
+ *
+ * Follow up for problem "Populating Next Right Pointers in Each Node".
+ *
+ * What if the given tree could be any binary tree? Would your previous solution still work?
+ *
+ * Note:
+ * You may only use constant extra space.
+ *
+ * For example,
+ *
+ * Given the following binary tree,
+ *          1
+ *        /  \
+ *       2    3
+ *      / \    \
+ *     4   5    7
+ *
+ * After calling your function, the tree should look like:
+ *           1 -> NULL
+ *         /  \
+ *        2 -> 3 -> NULL
+ *       / \    \
+ *      4-> 5 -> 7 -> NULL
+ *
+ **********************************************************************************/
+
 public class PopulatingNextRightPointersinEachNodeII {
     public void connect(TreeLinkNode root) {
         while (root != null) {
