73d5bd458439229e230a088419e1c905c06aa776
Mark Lin <m2linchen@outlook.com>
2015-12-01 14:55:48 +0800

    Add the Solution of "Unique Binary Search Trees"

---
+++ algorithms/java/src/UniqueBinarySearchTrees/UniqueBinarySearchTreesTest.java
@@ -0,0 +1,11 @@
+package UniqueBinarySearchTrees;
+
+/**
+ * Created by Mark Lin on 2015/12/1.
+ */
+public class UniqueBinarySearchTreesTest {
+    public static void main(String[] args) {
+        UniqueBinarySearchTrees nums = new UniqueBinarySearchTrees();
+        System.out.println(nums.numTrees(3));
+    }
+}
